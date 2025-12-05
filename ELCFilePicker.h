342f56905a09cf2b5672a650db6840e71561c9cf
JesseLade2 <demiomolola1@gmail.com>
2025-12-05 13:38:50 +0000

    Update ELCFilePicker.h

--- ELCFilePicker.h
+++ ELCFilePicker.h
@@ -11,6 +11,8 @@ namespace ELC {
 // filters = list of file extensions, e.g., {"png","gif","ase"}
 // returns the selected file path, or empty string if cancelled
 std::string FilePickerGUI(const std::string& folder,
-                          const std::vector<std::string>& filters);
+    const std::vector<std::string>& filters,
+    const std::string& title,
+    const std::string& description);
 
 }
